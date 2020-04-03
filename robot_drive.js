c2963c7450d68ca30ec8bf794a91d0e30c4d5458
AB-Cobb <andrew@cobbs-web.com>
2020-04-02 20:12:04 -0400

    webcam debugging

--- robot_drive.js
+++ robot_drive.js
@@ -1,20 +1,20 @@
+
 const GIPIO = require('onoff').Gpio
-const out_4 = new GIPIO(4, 'out')
 
 let motor_left = {
     pin1 : new GIPIO(2, 'out'),
     pin2 : new GIPIO(3,'out'),
     forward : function (params) {
         this.pin1.writeSync(1)
-        this.pin1.writeSync(0)
+        this.pin2.writeSync(0)
     },
     backward : function (params) {
         this.pin1.writeSync(0)
-        this.pin1.writeSync(1)
+        this.pin2.writeSync(1)
     },
     stop : function (params) {
         this.pin1.writeSync(0)
-        this.pin1.writeSync(0)
+        this.pin2.writeSync(0)
     }
 }
 let motor_right = {
@@ -22,29 +22,71 @@ let motor_right = {
     pin2 : new GIPIO(27,'out'),
     forward : function (params) {
         this.pin1.writeSync(1)
-        this.pin1.writeSync(0)
+        this.pin2.writeSync(0)
     },
     backward : function (params) {
         this.pin1.writeSync(0)
-        this.pin1.writeSync(1)
+        this.pin2.writeSync(1)
     },
     stop : function (params) {
         this.pin1.writeSync(0)
-        this.pin1.writeSync(0)
+        this.pin2.writeSync(0)
     }
 }
 
 
 const robot_serve_url ='https://robotserve.herokuapp.com/'
 const socketIO = require('socket.io-client')
+const socketIOstream = require('socket.io-stream')
+
+const { StreamCamera, Codec } = require ('pi-camera-connect')
+const webcamStream = new StreamCamera( {codac : Codec.H264} )
 
 console.log('RUNNING')
 
 socket = socketIO(robot_serve_url)
 socket.emit('PI_log', {txt: 'PI online... awaiting command'} )
+const fs = require ("fs");
 
+const runApp = async () => {
+
+    const streamCamera = new StreamCamera({
+        codec: Codec.H264
+    });
+
+    const videoStream = streamCamera.createStream();
+
+    const writeStream = fs.createWriteStream("video-stream_2.h264");
+
+    // Pipe the video stream to our video file
+    videoStream.pipe(writeStream);
+    console.log('awaiting cam start')
+    await streamCamera.startCapture();
+    console.log("cam is started")
+    // We can also listen to data events as they arrive
+    videoStream.on("data", data => console.log("New data", data));
+    videoStream.on("end", data => console.log("Video stream has ended"));
+
+    // Wait for 5 seconds
+    await new Promise(resolve => setTimeout(() => resolve(), 5000));
+
+    await streamCamera.stopCapture();
+};
+
+runApp();
+
+/*
+var outstream = socketIOstream.createStream()
+webcamStream.startCapture().then(() => {
+    console.log('cam stream started')
+    socketIOstream(socket).emit('PI_cam', outstream)
+    webcamStream.pipe(outstream)
+    webcamStream.on("data", data => console.log("New data", data));
+})
+
+//*/
 socket.on('cmd', data => {
-    console.log('command : ', data.drive)
+    //console.log('command : ', data.drive)
     //socket.emit('PI_log', {txt : "Revicived command" + data.drive})
     let drive = data.drive;
     switch (drive){
@@ -61,7 +103,7 @@ socket.on('cmd', data => {
             motor_right.backward()
             break;
         case 'lft':
-            console.log('Turning Left"')
+            console.log('Turning Left')
             socket.emit('PI_log', {txt : "Turning Left"})
             motor_left.backward()
             motor_right.forward()
